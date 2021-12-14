73deedd2e7b571530d1d40a490777ba65435895d
nguyenanhphat95 <nguyenanhphat95@gmail.com>
2021-12-14 11:37:33 +0700

    update api domain

--- consts/index.ts
+++ consts/index.ts
@@ -2,7 +2,7 @@ export const SIGNATURE =
   "QZOdawigBsrzaDYbl1SffM95uTINoOOxrLUWKYW78PDDizNYW4B8o8IqHXDpEDqZCR1UBW6aPrvYURu2R9LMI9tovI9TTvhA3b8puNrN2CMtg2pDSMd9NCOItXzWQv7mfuXNtFRmJHtTdWADDBsoak6PKbomAlmD8W3wj78AfTI=";
 export const PARTNER_ID = "GJOY";
 export const CLIENT_SECRET = "3b9f2a6e-8fcd-4243-b6f2-4faca5a81c31";
-export const API_DOMAIN = "127.0.0.1:22074";
+export const API_DOMAIN = "http://127.0.0.1:22074";
 
 export const GRANT_TYPE = "authorization_code";
 
