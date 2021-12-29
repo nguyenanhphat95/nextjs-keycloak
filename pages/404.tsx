918e806b76ce388e3873cefb9a3db5f8a85dd9ff
nguyenanhphat95 <nguyenanhphat95@gmail.com>
2021-12-29 09:24:03 +0700

    add not found page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+
+const NotFoundPage = () => {
+  useEffect(() => {
+    console.log("NotFoundPage");
+  }, []);
+  return <div>NotFoundPage</div>;
+};
+
+export default NotFoundPage;
